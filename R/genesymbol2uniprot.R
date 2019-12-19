1e0d9d60e202900cb007d430658a6871525842b2
enio23 <enio.gjerga@gmail.com>
2019-12-19 15:15:23 +0100

    GeneSymbol2Uniprot
    Updates over GeneSymbol2Uniprot function since it is used outside the main CARNIVAL analysis function

--- R/genesymbol2uniprot.R
+++ R/genesymbol2uniprot.R
@@ -1,11 +1,19 @@
-#'\code{GeneSymbol2Uniprot}
+'\code{GeneSymbol2Uniprot}
 #'
 #' This function converts the gene symbol rownames of a dataframe to uniprot 
 #' rownames. In case one gene symbol maps to two uniprot IDs, the row will be 
 #' duplicated.
 #'
+#' @param df A vector of genes.
+#' @param map A dataframe with a uniprot ID column and a genesymbol column.
+#' @param geneID Column index of the gene symbol column in map.
+#' @param uniprotID Column index of the uniprot column in map.
+#'
+#' @return A vector of genes or uniprot IDs.
+#' 
 #' @import dplyr
 #'
+#' @export
 
 GeneSymbol2Uniprot=function(df, map, geneID=1, uniprotID=2){
   
@@ -17,4 +25,3 @@ GeneSymbol2Uniprot=function(df, map, geneID=1, uniprotID=2){
   df<-df%>%dplyr::select(-uniprotids_for_mapping,-genesymbols_for_mapping)
   
 }
-
