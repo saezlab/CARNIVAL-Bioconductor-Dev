3e73320ce2674704d5fd6a78bbd4655e672501ca
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-01-21 10:15:13 +0100

    incremental debugging - fixing variables$reaction_variables missed variable renames

--- R/write_binaries_all_conditions.R
+++ R/write_binaries_all_conditions.R
@@ -5,27 +5,27 @@
 #'@return This code writes the list of binary variables (xp, xm, up & um).
 
 write_binaries_all_conditions <- function(variables=variables){
-  
+
   binaries <- c()
-  
+
   for(i in 1:length(variables)){
-    
+
     if(i != length(variables)){
-      
+
       binaries <- c(binaries, paste0("\t", variables[[i]]$variables[variables[[i]]$idxNodesUp]))
       binaries <- c(binaries, paste0("\t", variables[[i]]$variables[variables[[i]]$idxNodesDown]))
       binaries <- c(binaries, paste0("\t", variables[[i]]$variables[variables[[i]]$idxEdgesUp]))
       binaries <- c(binaries, paste0("\t", variables[[i]]$variables[variables[[i]]$idxEdgesDown]))
       binaries <- c(binaries, paste0("\t", variables[[i]]$variables[variables[[i]]$idxEdges]))
-      
+
     } else {
-      
-      binaries <- c(binaries, paste0("\t", variables[[i]]$Variables))
-      
+
+      binaries <- c(binaries, paste0("\t", variables[[i]]$variables))
+
     }
-    
+
   }
-  
+
   return(binaries)
-  
+
 }
--- R/write_boundaries_all_conditions.R
+++ R/write_boundaries_all_conditions.R
@@ -2,83 +2,83 @@
 #'
 #'
 #' This code writes the boundaries of each variable.
-#' 
+#'
 
 write_boundaries_all_conditions <- function(variables=variables, oF=oF){
-  
+
   M <- 100
-  
+
   bounds <- c()
-  
+
   for(i in 1:length(variables)){
-    
+
     if(i != length(variables)){
-      
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                    "-1 <= ", 
-                    variables[[i]]$variables[variables[[i]]$idxNodes], 
-                     " <= 1"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                    "0 <= ", 
-                    variables[[i]]$variables[variables[[i]]$idxNodesUp], 
-                    " <= 1"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                    "0 <= ", 
-                    variables[[i]]$variables[variables[[i]]$idxNodesDown], 
-                    " <= 1"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                    "0 <= ", 
-                    variables[[i]]$variables[variables[[i]]$idxEdgesUp], 
-                    " <= 1"))
-      bounds <- c(bounds, 
+
+      bounds <- c(bounds,
                   paste0("\t",
-                         "0 <= ", 
-                         variables[[i]]$variables[variables[[i]]$idxEdgesDown], 
+                    "-1 <= ",
+                    variables[[i]]$variables[variables[[i]]$idxNodes],
+                     " <= 1"))
+      bounds <- c(bounds,
+                  paste0("\t",
+                    "0 <= ",
+                    variables[[i]]$variables[variables[[i]]$idxNodesUp],
+                    " <= 1"))
+      bounds <- c(bounds,
+                  paste0("\t",
+                    "0 <= ",
+                    variables[[i]]$variables[variables[[i]]$idxNodesDown],
+                    " <= 1"))
+      bounds <- c(bounds,
+                  paste0("\t",
+                    "0 <= ",
+                    variables[[i]]$variables[variables[[i]]$idxEdgesUp],
+                    " <= 1"))
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "0 <= ",
+                         variables[[i]]$variables[variables[[i]]$idxEdgesDown],
                          " <= 1"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                         "-1 <= ", 
-                         variables[[i]]$variables[variables[[i]]$idxB], 
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "-1 <= ",
+                         variables[[i]]$variables[variables[[i]]$idxB],
                          " <= 1"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                         "0 <= ", 
-                         variables[[i]]$variables[variables[[i]]$idxDist], 
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "0 <= ",
+                         variables[[i]]$variables[variables[[i]]$idxDist],
                          " <= ", M))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                         "0 <= ", 
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "0 <= ",
                          unique(
                            strsplit(
-                             oF, 
+                             oF,
                              split = " ")[[1]][grep(
-                               pattern = "absDiff", 
-                               x = strsplit(oF, split = " ")[[1]])]), 
+                               pattern = "absDiff",
+                               x = strsplit(oF, split = " ")[[1]])]),
                          " <= 2"))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                         "0 <= ", 
-                         variables[[i]]$variables[variables[[i]]$idxDist], 
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "0 <= ",
+                         variables[[i]]$variables[variables[[i]]$idxDist],
                          " <= ", M))
-      bounds <- c(bounds, 
-                  paste0("\t", 
-                         "0 <= ", 
-                         variables[[i]]$variables[variables[[i]]$idxEdges], 
+      bounds <- c(bounds,
+                  paste0("\t",
+                         "0 <= ",
+                         variables[[i]]$variables[variables[[i]]$idxEdges],
                          " <= 1"))
-      
+
     } else {
-      
-      bounds <- c(bounds, paste0("\t", "0 <= ", 
-                                 variables[[i]]$Variables, " <= 1"))
-      
+
+      bounds <- c(bounds, paste0("\t", "0 <= ",
+                                 variables[[i]]$variables, " <= 1"))
+
     }
-    
+
   }
-  
+
   return(bounds)
-  
+
 }
