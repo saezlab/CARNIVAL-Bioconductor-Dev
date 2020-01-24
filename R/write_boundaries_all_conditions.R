c4fdc89c8a08ea2a666413160577b57465e8ee72
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-01-24 09:53:11 +0100

    added comments

--- R/write_boundaries_all_conditions.R
+++ R/write_boundaries_all_conditions.R
@@ -14,41 +14,56 @@ write_boundaries_all_conditions <- function(variables=variables, oF=oF){
 
     if(i != length(variables)){
 
+      # -1 <= x_(j,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                     "-1 <= ",
                     variables[[i]]$variables[variables[[i]]$idxNodes],
                      " <= 1"))
+
+      # 0 <= x^+_(j,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                     "0 <= ",
                     variables[[i]]$variables[variables[[i]]$idxNodesUp],
                     " <= 1"))
+
+      # 0 <= x^-_(j,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                     "0 <= ",
                     variables[[i]]$variables[variables[[i]]$idxNodesDown],
                     " <= 1"))
+
+      # 0 <= u^+_(i,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                     "0 <= ",
                     variables[[i]]$variables[variables[[i]]$idxEdgesUp],
                     " <= 1"))
+
+      # 0 <= u^-_(i,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                          "0 <= ",
                          variables[[i]]$variables[variables[[i]]$idxEdgesDown],
                          " <= 1"))
+
+      # -1 <= B_(j,k) <= 1
       bounds <- c(bounds,
                   paste0("\t",
                          "-1 <= ",
                          variables[[i]]$variables[variables[[i]]$idxB],
                          " <= 1"))
+
+      # 0 <= d_j <= M
       bounds <- c(bounds,
                   paste0("\t",
                          "0 <= ",
                          variables[[i]]$variables[variables[[i]]$idxDist],
                          " <= ", M))
+
+      # 0 <= A_(j,k) <= 2
       bounds <- c(bounds,
                   paste0("\t",
                          "0 <= ",
@@ -59,19 +74,25 @@ write_boundaries_all_conditions <- function(variables=variables, oF=oF){
                                pattern = "absDiff",
                                x = strsplit(oF, split = " ")[[1]])]),
                          " <= 2"))
-      bounds <- c(bounds,
-                  paste0("\t",
-                         "0 <= ",
-                         variables[[i]]$variables[variables[[i]]$idxDist],
-                         " <= ", M))
-      bounds <- c(bounds,
-                  paste0("\t",
-                         "0 <= ",
-                         variables[[i]]$variables[variables[[i]]$idxEdges],
-                         " <= 1"))
+
+      # Redundant 0 <= d_j <= M
+      # bounds <- c(bounds,
+      #             paste0("\t",
+      #                    "0 <= ",
+      #                    variables[[i]]$variables[variables[[i]]$idxDist],
+      #                    " <= ", M))
+
+      # Disabled not needed at the moment
+      # Was used for additional variables for each node in multi cond CARNIVAL
+      # bounds <- c(bounds,
+      #             paste0("\t",
+      #                    "0 <= ",
+      #                    variables[[i]]$variables[variables[[i]]$idxEdges],
+      #                    " <= 1"))
 
     } else {
 
+      # 0 <= y_(i,t) <= 1
       bounds <- c(bounds, paste0("\t", "0 <= ",
                                  variables[[i]]$variables, " <= 1"))
 
