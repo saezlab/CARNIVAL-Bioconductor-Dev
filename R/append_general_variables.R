ef72b98dcf8230abd7550e2a862fdd0052451cf9
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-01-24 10:22:27 +0100

    creating only y variables for case dt

--- R/append_general_variables.R
+++ R/append_general_variables.R
@@ -6,41 +6,6 @@
 append_general_variables <- function(variables = variables,
                                      dt = dt){
 
-  for(ii in 1:length(variables)){
-
-    cnt <-
-      length(variables[[ii]]$idxNodes) +
-      length(variables[[ii]]$idxNodesUp) +
-      length(variables[[ii]]$idxNodesDown) +
-      length(variables[[ii]]$idxEdgesUp) +
-      length(variables[[ii]]$idxEdgesDown) + 1
-
-    # Add an additional CPLEX variable for all EdgesUp
-    var2append <- paste0("xb",
-                         cnt:(cnt+length(variables[[ii]]$idxEdgesUp)-1),
-                         "_", ii)
-    # Explanations for the newly created variables
-    exp2append <- gsub(pattern = "ReactionUp ",
-                       replacement = "Reaction ",
-                       x = variables[[ii]]$exp[variables[[ii]]$idxEdgesUp],
-                       " in experiment ",
-                       ii)
-
-    # Get indices of the newly created variables in the large variables field
-    # and write the indices to the new field idxEdges
-    idxEdges <-
-      (length(variables[[ii]]$variables) + 1):(length(variables[[ii]]$variables) +
-                                                 length(var2append))
-    namesVar <- names(variables[[ii]])
-    variables[[ii]]$variables <- c(variables[[ii]]$variables, var2append)
-    variables[[ii]]$exp <- c(variables[[ii]]$exp, exp2append)
-
-    variables[[ii]][[length(variables[[ii]])+1]] <- idxEdges
-
-    names(variables[[ii]]) <- c(namesVar, "idxEdges")
-
-  }
-
   var = variables
 
   # append a field for reaction variables
@@ -53,6 +18,7 @@ append_general_variables <- function(variables = variables,
 
     for(ii in 1:length(variables)){
       # create y reaction variables from number of reactions
+      # y_(i,t)
       y_cplex <- list(y_cplex,
                       paste0("y",
                              1:length(variables[[ii]]$idxEdgesUp),
@@ -69,6 +35,42 @@ append_general_variables <- function(variables = variables,
     var[[length(var)]][[length(var[[length(var)]]) + 1]] <- unlist(exp_y)
 
   } else {
+
+    for(ii in 1:length(variables)){
+
+      cnt <-
+        length(variables[[ii]]$idxNodes) +
+        length(variables[[ii]]$idxNodesUp) +
+        length(variables[[ii]]$idxNodesDown) +
+        length(variables[[ii]]$idxEdgesUp) +
+        length(variables[[ii]]$idxEdgesDown) + 1
+
+      # Add an additional CPLEX variable for all EdgesUp
+      var2append <- paste0("xb",
+                           cnt:(cnt+length(variables[[ii]]$idxEdgesUp)-1),
+                           "_", ii)
+      # Explanations for the newly created variables
+      exp2append <- gsub(pattern = "ReactionUp ",
+                         replacement = "Reaction ",
+                         x = variables[[ii]]$exp[variables[[ii]]$idxEdgesUp],
+                         " in experiment ",
+                         ii)
+
+      # Get indices of the newly created variables in the large variables field
+      # and write the indices to the new field idxEdges
+      idxEdges <-
+        (length(variables[[ii]]$variables) + 1):(length(variables[[ii]]$variables) +
+                                                   length(var2append))
+      namesVar <- names(variables[[ii]])
+      variables[[ii]]$variables <- c(variables[[ii]]$variables, var2append)
+      variables[[ii]]$exp <- c(variables[[ii]]$exp, exp2append)
+
+      variables[[ii]][[length(variables[[ii]])+1]] <- idxEdges
+
+      names(variables[[ii]]) <- c(namesVar, "idxEdges")
+
+    }
+
     # create y reaction variables from number of reactions
     var[[length(var)]][[length(var[[length(var)]])+1]] <-
       paste0("y", 1:length(variables$Condition_1$idxEdgesUp))
