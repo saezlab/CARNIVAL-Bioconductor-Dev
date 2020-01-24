a5cb98df3048d94d0708050744a4b57b0394ae7d
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-01-24 13:09:38 +0100

    added comments

--- R/write_constraints_1_all.R
+++ R/write_constraints_1_all.R
@@ -1,18 +1,22 @@
 #'\code{write_constraints_1_all}
 #'
-#' This code writes the list of constraints (1) of the ILP problem for all the 
+#' This code writes the list of constraints (1) of the ILP problem for all the
 #' conditions.
-#' 
+#'
+#' u^+_(i,k) >= sigma_i * x_(j,k)
+#'
 
 write_constraints_1_all <- function(variables=variables){
 
   constraints1 <- c()
 
+  # for each experiment / time
   for(i in 1:length(variables)){
 
     var <- variables[[i]]
 
-    constraints1 <- c(constraints1, write_constraints_1(variables = var, 
+    # create constraint 1
+    constraints1 <- c(constraints1, write_constraints_1(variables = var,
                                                         conditionIDX = i))
 
   }
