839c91ae1482895d2ff03bc0cd01dbac2da08b74
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-01-24 15:44:50 +0100

    added in code representations for test data

--- test_carnival_dt.R
+++ test_carnival_dt.R
@@ -11,17 +11,26 @@ library(igraph) # load igraph library
 library(readxl) # load readxl library
 library(tidyr)
 
-base_directory <- "D:/MoBi_Studium/Master/internships/saez/gitRepos/CARNIVAL_dt/"
-data_directory <- "D:/MoBi_Studium/Master/internships/saez/gitRepos/CARNIVAL_dt/data/test_data/"
+# base_directory <- "D:/MoBi_Studium/Master/internships/saez/gitRepos/CARNIVAL_dt/"
+# data_directory <- "D:/MoBi_Studium/Master/internships/saez/gitRepos/CARNIVAL_dt/data/test_data/"
 solver_path <- "C:/Program Files/IBM/ILOG/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
 
-test_network <- readr::read_tsv(paste0(data_directory, "test_network.tsv"))
-test_measurements <- readr::read_tsv(paste0(data_directory, "test_measurements.tsv"))
-test_input <- readr::read_tsv(paste0(data_directory, "test_input.tsv"))
+# test_network <- readr::read_tsv(paste0(data_directory, "test_network.tsv"))
+# test_measurements <- readr::read_tsv(paste0(data_directory, "test_measurements.tsv"))
+# test_input <- readr::read_tsv(paste0(data_directory, "test_input.tsv"))
 
 # test_network <- readr::read_tsv(paste0(data_directory, "test_network_big.tsv"))
 # test_measurements <- readr::read_tsv(paste0(data_directory, "test_measurements_big.tsv"))
 # test_input <- readr::read_tsv(paste0(data_directory, "test_input_big.tsv"))
+
+test_network <- tibble(Source = c("P", "N1", "M1", "N2", "M2", "N3"),
+                       Effect = c(1, 1, -1, 1, 1, 1),
+                       Target = c("N1", "M1", "N2", "M2", "N3", "N1"))
+
+test_measurements <- tibble(M1 = c(1, -1),
+                            M2 = c(-1, 1))
+
+test_input <- tibble(P = c(1))
 
 # cplex solver
 toy_result <- runCARNIVAL(solverPath = solver_path,
